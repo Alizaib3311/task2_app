2467a29324f3e643aaf2417417e64d73910b12f3
Alizaib3311 <91749469+Alizaib3311@users.noreply.github.com>
2021-10-24 17:01:45 +0500

    second commit

---
+++ app/src/main/java/com/example/ali/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
