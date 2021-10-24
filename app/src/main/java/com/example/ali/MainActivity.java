ae7253075d67febb45cd63081c56778ae86eb8d2
Alizaib3311 <91749469+Alizaib3311@users.noreply.github.com>
2021-10-24 17:08:21 +0500

    layou commit

--- app/src/main/java/com/example/ali/MainActivity.java
+++ app/src/main/java/com/example/ali/MainActivity.java
@@ -13,8 +13,13 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    Spinner meat,veg,dessert;
+    Button cal;
+    TextView res;
+    String[] data={"Chicken","Beef","Mutton","Fish"};
+    ArrayList<String> result=new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
